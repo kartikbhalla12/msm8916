b59633f0248f5fbd1bff06027714978dd44febe1
The-Demon12 <kartikbhalla12@gmail.com>
2016-08-20 07:37:40 +0000

    GPU: Overclock to 550 MHz (3/5)
    Signed-off-by: The-Demon12 <kartikbhalla12@gmail.com>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -566,7 +566,7 @@ static struct rcg_clk vfe0_clk_src = {
 		.dbg_name = "vfe0_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			465000000),
+			550000000),
 		CLK_INIT(vfe0_clk_src.c),
 	},
 };
